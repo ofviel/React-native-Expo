439e7234d3b703bf158d449e41aa0204852aefe4
Vitor Gabriel <vitorggsilva@fac.pe.senac.br>
2023-06-02 20:33:20 -0300

    first commit

---
+++ src/screens/Home/styles.ts
@@ -0,0 +1,34 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: "#1e1e1e",
+    },
+    header:{                    
+      padding: 25,
+    },
+    headerText:{
+    marginTop: 30,
+    fontSize: 24,
+    lineHeight: 45,
+    color: "#B0E0E6"
+  },
+  containerInput: {
+    backgroundColor: "#87CEEB",
+    height: 42,
+    padding: 10,
+    borderRadius: 16,
+    marginTop: 24,
+    marginBottom: 20,
+    alignItems: "center",
+    justifyContent: "space-between",
+    flexDirection: "row"
+  },
+  input: {
+   color: "#E0FFFF", 
+   width: "80%",
+   paddingLeft: 15,
+  },
+  });
+  
